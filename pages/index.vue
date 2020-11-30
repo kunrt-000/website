051f51ab1a1e6446a3e32427b27b26c817346d79
Aman Harwara <amanharwara@protonmail.com>
2020-11-30 13:21:09 +0530

    Initial Nuxtjs rewrite commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>hi</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: "Aman Harwara",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Aman Harwara is a front-end web developer, designer and musician."
+        }
+      ]
+    }
+  }
+})
+</script>
+
+<style>
+</style>
