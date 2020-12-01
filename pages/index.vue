5e22edd492b848df97c9679a8bf6f2b01dd2441d
Aman Harwara <amanharwara@protonmail.com>
2020-12-01 14:11:06 +0530

    Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,37 @@
 <template>
-  <h1>hi</h1>
+  <div id="home">
+    <div class="section" id="main-section">
+      <Header page="home" />
+      <Hero />
+    </div>
+    <AboutSection />
+    <WorkSection />
+    <Footer />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({
-  head() {
-    return {
-      title: "Aman Harwara",
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: "Aman Harwara is a front-end web developer, designer and musician."
-        }
-      ]
-    }
-  }
-})
+export default Vue.extend({})
 </script>
 
-<style>
+<style lang="scss">
+body::after {
+  content: '';
+  display: block;
+  background: #fbe6cd;
+  height: 100vh;
+  width: 54%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: -1;
+}
+</style>
+
+<style lang="scss" scoped>
+#main-section {
+  padding: 2.5vw 7.25vw 2vw;
+}
 </style>
