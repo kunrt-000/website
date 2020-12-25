530017d51bd08e9616a5b078c9758407543a4361
Aman Harwara <amanharwara@protonmail.com>
2020-12-25 22:27:49 +0530

    Add descriptions to projects

--- data/projects.ts
+++ data/projects.ts
@@ -5,8 +5,10 @@ const projects: Project[] = [
     link: 'https://github.com/amanharwara/altus',
     category: 'Desktop App',
     workType: 'Design / Development',
-    description:
-      'Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!\n\nIt is made using the Electron framework with HTML, CSS and vanilla JavaScript.',
+    description: `Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!<br/><br/>
+    It is made using the Electron framework with HTML, CSS and vanilla JavaScript.<br/><br/>
+    I started creating it in the latter half of 2018 because no other desktop client for WhatsApp had a dark theme or an option for custom themes.<br/><br/>
+    Altus is still regularly maintained, with over 39,000 downloads as of writing.`,
     screenshots: ['1.png', '2.png'],
   },
   {
@@ -20,24 +22,15 @@ const projects: Project[] = [
     screenshots: ['1.png', '2.png'],
   },
   {
-    id: 'altus3',
-    title: 'Altus',
-    link: 'https://github.com/amanharwara/altus',
-    category: 'Desktop App',
+    id: 'arm64',
+    title: 'Arm-64.com',
+    link: 'https://arm-64.com',
+    category: 'Website / Blog',
     workType: 'Design / Development',
-    description:
-      'Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!\n\nIt is made using the Electron framework with HTML, CSS and vanilla JavaScript.',
-    screenshots: ['1.png', '2.png'],
-  },
-  {
-    id: 'altus4',
-    title: 'Altus',
-    link: 'https://github.com/amanharwara/altus',
-    category: 'Desktop App',
-    workType: 'Design / Development',
-    description:
-      'Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!\n\nIt is made using the Electron framework with HTML, CSS and vanilla JavaScript.',
-    screenshots: ['1.png', '2.png'],
+    description: `Arm-64.com is a blog run by experts at Fosshost about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations.<br/><br/>
+    It is completely built with Next.js + Typescript using a flat-file approach and being mostly self-contained without using too many other dependencies.<br/><br/>
+    For the design, I went with a modern and clean look while remembering to maintain familiarity and good UX. A healthy amount of white space is used to give the design some room to breath and to keep the user's information overload to the minimal.`,
+    screenshots: ['1.png', '2.png', '3.png'],
   },
 ]
 
