3e62909f22e3b79dc61b7c23fd545e5f27e75efc
Aman Harwara <amanharwara@protonmail.com>
2020-12-30 21:52:17 +0530

    Move background style to index page instead of global styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,5 @@
 <template>
   <div id="home">
-    <style>
-      @media screen and (min-width: 768px) {
-        body::after {
-          content: '';
-          display: block !important;
-          background: #1b1b1b;
-          background: var(--alt-bg-1);
-          height: max(40rem, 50vh);
-          width: 50vw;
-          position: absolute;
-          top: 0;
-          right: 0;
-          z-index: -1;
-        }
-      }
-    </style>
     <Header page="home" />
     <main class="section" id="main-section">
       <Hero />
@@ -41,6 +25,19 @@ main {
   main {
     padding: 2rem 5rem 6rem;
     padding: max(2rem, 10vh) 5rem max(6rem, 20vh);
+
+    &::after {
+      content: '';
+      display: block !important;
+      background: #1b1b1b;
+      background: var(--alt-bg-1);
+      height: max(40rem, 50vh);
+      width: 50vw;
+      position: absolute;
+      top: 0;
+      right: 0;
+      z-index: -1;
+    }
   }
 }
 </style>
