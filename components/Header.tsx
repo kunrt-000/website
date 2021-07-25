e43c1bf5f125eebe0610af7911dc78129067f1c3
Aman Harwara <amanharwara@protonmail.com>
2021-07-25 14:24:38 +0530

    Add active states to nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,48 @@
 import styles from "@/styles/Header.module.scss";
 import Link from "next/link";
 import MenuIcon from "./icons/MenuIcon";
+import { useRouter } from "next/router";
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <Link href="/" passHref={true}>
-          Aman Harwara
-        </Link>
-      </div>
-      <div className={styles.right}>
-        <nav className={styles.nav}>
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.left}>
           <Link href="/" passHref={true}>
-            <a className={styles.item}>Home</a>
+            Aman Harwara
           </Link>
-          <Link href="/#work" passHref={true}>
-            <a className={styles.item}>Work</a>
-          </Link>
-          <Link href="/music" passHref={true}>
-            <a className={styles.item}>Music</a>
-          </Link>
-          <Link href="/blog" passHref={true}>
-            <a className={styles.item}>Blog</a>
-          </Link>
-        </nav>
-        <div className={styles.menuButton}>
-          <MenuIcon />
+        </div>
+        <div className={styles.right}>
+          <nav className={styles.nav}>
+            <Link href="/" passHref={true}>
+              <a className={styles.item} data-active={router.pathname === "/"}>
+                Home
+              </a>
+            </Link>
+            <Link href="/#work" passHref={true}>
+              <a className={styles.item}>Work</a>
+            </Link>
+            <Link href="/music" passHref={true}>
+              <a
+                className={styles.item}
+                data-active={router.pathname.includes("/music")}
+              >
+                Music
+              </a>
+            </Link>
+            <Link href="/blog" passHref={true}>
+              <a className={styles.item}>Blog</a>
+            </Link>
+          </nav>
+          <div className={styles.menuButton}>
+            <MenuIcon />
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
