2c799a6a66861254710950d33d36b197595ea199
Aman Harwara <amanharwara@protonmail.com>
2021-07-26 20:00:16 +0530

    Add hero section images

--- components/index/HeroSection.tsx
+++ components/index/HeroSection.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/index/HeroSection.module.scss";
 import Button from "../Button";
+import Img from "react-optimized-image";
 
 const HeroSection = () => (
   <section className={styles.heroSection}>
@@ -21,7 +22,31 @@ const HeroSection = () => (
         </a>
       </div>
     </div>
-    <div className={styles.right}></div>
+    <div className={styles.right}>
+      <div className={styles.rect}>
+        <div className={styles.center}>
+          <Img
+            src={require(`../../public/icons/altus/altus.svg`)}
+            alt="Altus logo"
+          />
+        </div>
+      </div>
+      <div className={styles.rect}>
+        <Img
+          webp
+          src={require(`../../public/img/music/moodbender.png`)}
+          alt="Moodbender cover"
+        />
+      </div>
+      <div className={styles.rect}>
+        <div className={styles.center}>
+          <Img
+            src={require(`../../public/icons/chartr/chartr.svg`)}
+            alt="Altus logo"
+          />
+        </div>
+      </div>
+    </div>
   </section>
 );
 
