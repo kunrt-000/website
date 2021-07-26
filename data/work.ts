4fc6878ae211dafd163f89980f573b0885214733
Aman Harwara <amanharwara@protonmail.com>
2021-07-26 16:06:02 +0530

    Remove explicit line-breaks

--- data/work.ts
+++ data/work.ts
@@ -2,13 +2,13 @@ const work: WorkProject[] = [
   {
     id: "altus",
     title: "Altus",
-    website: "https://github.com/amanharwara/altus",
+    website: "https://amanharwara.com/altus",
     techStack: "Electron + Svelte + Typescript",
     summary:
       "Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!",
-    description: `Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!<br/><br/>
-  It is made using the Electron framework with HTML, CSS and vanilla JavaScript.<br/><br/>
-  I started creating it in the latter half of 2018 because no other desktop client for WhatsApp had a dark theme or an option for custom themes.<br/><br/>
+    description: `Altus is a free, open-source desktop client for WhatsApp with built-in themes and multiple account support, available for Windows, Mac & Linux!
+  It is made using the Electron framework with HTML, CSS and vanilla JavaScript.
+  I started creating it in the latter half of 2018 because no other desktop client for WhatsApp had a dark theme or an option for custom themes.
   Altus is still regularly maintained, with over 39,000 downloads as of writing.`,
   },
   {
@@ -28,8 +28,8 @@ const work: WorkProject[] = [
     techStack: "Next.js + Typescript",
     summary:
       "AArch64.com is a blog run by experts at Fosshost about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations.",
-    description: `AArch64.com is a blog run by experts at Fosshost about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations.<br/><br/>
-  It is completely built with Next.js + Typescript using a flat-file approach and being mostly self-contained without using too many other dependencies.<br/><br/>
+    description: `AArch64.com is a blog run by experts at Fosshost about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations.
+  It is completely built with Next.js + Typescript using a flat-file approach and being mostly self-contained without using too many other dependencies.
   For the design, I went with a modern and clean look while remembering to maintain familiarity and good UX. A healthy amount of white space is used to give the design some room to breath and to keep the user's information overload to the minimal.`,
   },
 ];
